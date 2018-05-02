ec4e09ca4fc20dff24bca4a750344a1ee0d7f58b
clotte <caro-lot@hotmail.com>
2018-05-02 13:10:50 +0200

    Pilot Evaluation Data cleaning

--- pilotDataAnalysis.R
+++ pilotDataAnalysis.R
@@ -39,13 +39,57 @@ data <- data_raw %>%
     TargetIsHit = Succ
   )
 
+
 # Renaming of cell values in a more meaningful way
 data$Condition[data$Condition == '0'] <- "Regular Edge"
 data$Condition[data$Condition == '1'] <- "Virtual Edge"
 data$Block[data$Block == '0'] <- "Demo"
 data$TargetIsHit[data$TargetIsHit == '0'] <- "FALSE"
 data$TargetIsHit[data$TargetIsHit == '1'] <- "TRUE"
+data$TargetHighlighted <- as.POSIXct(data$TargetHighlighted, format = "%d-%m-%Y %H:%M:%S:%OS")
+data$TargetClicked <- as.POSIXct(data$TargetClicked, format = "%d-%m-%Y %H:%M:%S:%OS")
 
+# Option to display the ms as well. -> Doesn't work properly yet...
+options(digits.secs = 3)
+
+# Remove all Demo Trial from the data set
+data <- data %>%
+  filter(Block != "Demo")
+
+# Get first Timestamp (TargetHighlighted) and last Timestamp (TargetClicked) of a sequence and calculate the difference ==> Movement Time
+data_per_sequence <- data %>%
+  select(
+    Participant,
+    Condition,
+    Block,
+    TrialNumber,
+    TargetNumber,
+    TargetHighlighted,
+    TargetClicked
+    ) %>%
+  filter(TargetNumber == 1 | TargetNumber ==5)
+
+
+
+# Calculate Error_Rate for each sequence
+
+
+# Add all calculated Movement_Times together to get the total time for one pointing task trial
+
+# Calculate mean of Error_Rate over one whole pointing task trial
+
+
+
+data <- mutate(data, movementTime = as.numeric(TargetClicked - TargetHighlighted, unit ="secs"))
+
+
+
+#
+# data$TargetHighlighted[as.Date(data$TargetHighlighted, "hh:mm:ss:*")]
+#
+
+# Calculate Movement Time per Sequence
+# mutate(data, movementTime = TargetClicked - TargetHighlighted, unit="secs")
 
 
 
